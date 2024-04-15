73ef90e198177b762e8adabbda80cab3dbeb0a8c
anadolsk <anadolsk@macalester.edu>
2024-04-15 10:19:50 -0500

    gamepiece

---
+++ src/GamePiece.java
@@ -0,0 +1,11 @@
+import edu.macalester.graphics.Image;
+
+public class GamePiece {
+    private enum PieceType {
+        ROCK,
+        PAPER, 
+        SCISSORS; 
+    }
+    private Image image;
+}
+
