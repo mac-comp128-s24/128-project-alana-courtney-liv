6ba38b7b6b2e5148993a0cfc92074ee2c7de0b92
Courtney Brown <116745836+cornedtea@users.noreply.github.com>
2024-04-17 10:44:05 -0500

    add movement function and extend Image

--- src/GamePiece.java
+++ src/GamePiece.java
@@ -1,41 +1,63 @@
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Image;
+import edu.macalester.graphics.Point;
 
-public class GamePiece {
+public class GamePiece extends Image {
     public enum PieceType {
         ROCK,
         PAPER, 
         SCISSORS; 
     }
-    private Image image;
+    private static final double WANDER_FROM_CENTER = 60000;
+    private static final double WIGGLINESS = 0.2;
+    private double direction = 0;
     private PieceType type;
-    private double maxX;
-    private double maxY;
-    
 
     public GamePiece(PieceType type, CanvasWindow canvas, double minY) {
+        super(0,0);
         this.type = type;
-        setImage();
-        canvas.add(image);
+        setPath();
     }
 
-    private void setImage() {
+    private void setPath() {
         switch (type) {
             case ROCK :
-                image = new Image("rock.png");
+                setImagePath("rock.png");
                 break;
             case PAPER :
-                image = new Image("paper.png");
+                setImagePath("paper.png");
                 break;
             case SCISSORS :
-                image = new Image("scissors.png");
+                setImagePath("scissors.png");
                 break;
         }
     }
 
+    public void updatePosition(Point centerOfGravity, double speed) {
+        moveBy(Math.cos(direction) * speed, Math.sin(direction) * speed);
+
+        double distToCenter = getCenter().distance(centerOfGravity);
+        double angleToCenter = centerOfGravity.subtract(getCenter()).angle();
+        double turnTowardCenter = normalizeRadians(angleToCenter - direction);
+
+        direction = normalizeRadians(
+            direction
+                + (Math.random() - 0.5) * (WIGGLINESS * (speed / 5))
+                + turnTowardCenter * Math.tanh(distToCenter / (WANDER_FROM_CENTER / speed)));
+    }
+
+    private static double normalizeRadians(double theta) {
+        double pi2 = Math.PI * 2;
+        return ((theta + Math.PI) % pi2 + pi2) % pi2 - Math.PI;
+    }
+
     public void changeType(PieceType newType) {
         type = newType;
-        setImage();
+        setPath();
+    }
+
+    public PieceType getType() {
+        return type;
     }
 }
 
