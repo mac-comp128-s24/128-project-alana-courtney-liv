7faa12772600f593e5e39beff7aace65697fdbc1
Courtney Brown <116745836+cornedtea@users.noreply.github.com>
2024-04-19 09:58:24 -0500

    sizes to user screen

--- src/RPS.java
+++ src/RPS.java
@@ -1,17 +1,29 @@
+import java.awt.Toolkit;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
 
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.GraphicsObject;
 
 public class RPS {
+    private final int WINDOW_WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth() - 50;
+    private final int WINDOW_HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight() - 50;
+
     private CanvasWindow canvas;
+    private GraphicsGroup pieceField;
     private HashMap<GamePiece.PieceType, Integer> teamCounts;
     private HashSet<GamePiece> pieces;
+    private UI ui;
 
     public RPS() {
-        canvas = new CanvasWindow("Rock Paper Scissors", 600, 600);
+        canvas = new CanvasWindow("Rock Paper Scissors", WINDOW_WIDTH, WINDOW_HEIGHT);
+        pieceField = new GraphicsGroup();
+        canvas.add(pieceField);
+        ui = new UI();
+        canvas.add(ui, 0, 50);
         teamCounts = new HashMap<>();
         teamCounts.put(GamePiece.PieceType.ROCK, 3);
         teamCounts.put(GamePiece.PieceType.PAPER, 3);
@@ -24,20 +36,20 @@ public class RPS {
         Random r = new Random();
         for (int i = 0; i < teamCounts.get(GamePiece.PieceType.ROCK); i++) {
             GamePiece tempRock = new GamePiece(GamePiece.PieceType.ROCK, canvas, 0);
-            canvas.add(tempRock, r.nextDouble(0, canvas.getWidth()), r.nextDouble(0, canvas.getHeight()));
-            tempRock.setMaxHeight(canvas.getHeight() / (4 * teamCounts.get(GamePiece.PieceType.ROCK)));
+            pieceField.add(tempRock, r.nextDouble(0, WINDOW_WIDTH), r.nextDouble(0, WINDOW_HEIGHT));
+            tempRock.setMaxHeight(WINDOW_HEIGHT / (4 * teamCounts.get(GamePiece.PieceType.ROCK)));
             pieces.add(tempRock);
         }
         for (int i = 0; i < teamCounts.get(GamePiece.PieceType.PAPER); i++) {
             GamePiece tempPaper = new GamePiece(GamePiece.PieceType.PAPER, canvas, 0);
-            canvas.add(tempPaper, r.nextDouble(0, canvas.getWidth()), r.nextDouble(0, canvas.getHeight()));
-            tempPaper.setMaxHeight(canvas.getHeight() / (4 * teamCounts.get(GamePiece.PieceType.PAPER)));
+            pieceField.add(tempPaper, r.nextDouble(0, WINDOW_WIDTH), r.nextDouble(0, WINDOW_HEIGHT));
+            tempPaper.setMaxHeight(WINDOW_HEIGHT / (4 * teamCounts.get(GamePiece.PieceType.PAPER)));
             pieces.add(tempPaper);
         }
         for (int i = 0; i < teamCounts.get(GamePiece.PieceType.SCISSORS); i++) {
             GamePiece tempScissors = new GamePiece(GamePiece.PieceType.SCISSORS, canvas, 0);
-            canvas.add(tempScissors, r.nextDouble(0, canvas.getWidth()), r.nextDouble(0, canvas.getHeight()));
-            tempScissors.setMaxHeight(canvas.getHeight() / (4 * teamCounts.get(GamePiece.PieceType.SCISSORS)));
+            pieceField.add(tempScissors, r.nextDouble(0, WINDOW_WIDTH), r.nextDouble(0, WINDOW_HEIGHT));
+            tempScissors.setMaxHeight(WINDOW_HEIGHT / (4 * teamCounts.get(GamePiece.PieceType.SCISSORS)));
             pieces.add(tempScissors);
         }
     }
@@ -57,7 +69,7 @@ public class RPS {
 
     private void handleCollisions() {
         for (GamePiece piece : pieces) {
-            GraphicsObject touching = canvas.getElementAt(piece.getPosition());
+            GraphicsObject touching = pieceField.getElementAt(piece.getPosition());
             if (touching != null && touching.getClass() == GamePiece.class && !touching.equals(piece)) {
                 
             }
