6daa769b90496adbe66f5f1eba47c1b68c88d35d
Courtney Brown <116745836+cornedtea@users.noreply.github.com>
2024-04-19 09:58:15 -0500

    ui placeholder

--- src/UI.java
+++ src/UI.java
@@ -1,5 +1,9 @@
 import edu.macalester.graphics.GraphicsGroup;
+import edu.macalester.graphics.GraphicsText;
 
-public class UI extends GraphicsGroup{
+public class UI extends GraphicsGroup {
     
+    public UI(){
+        this.add(new  GraphicsText("UI goes here"));
+    }
 }
